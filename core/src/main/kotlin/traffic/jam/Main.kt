54fd52eb38ce8ae80de3f8baafc79ba0abef3f24
Julien Bertozzi <bertozzijulien@gmail.com>
2022-11-14 20:44:03 +0100

    initial commit, display a white square

---
+++ core/src/main/kotlin/traffic/jam/Main.kt
@@ -0,0 +1,30 @@
+package traffic.jam
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class Main : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var image: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        image = Texture("square.png")
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(image, 140f, 210f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        image.dispose()
+    }
+}
