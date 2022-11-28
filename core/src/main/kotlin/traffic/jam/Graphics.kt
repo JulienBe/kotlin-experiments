5ce326089f1c7625ec95cb5e8759a4d0a3595011
Julien Bertozzi <bertozzijulien@gmail.com>
2022-11-28 21:59:33 +0100

    pixel perfect

---
+++ core/src/main/kotlin/traffic/jam/Graphics.kt
@@ -0,0 +1,53 @@
+package traffic.jam
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.*
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.FrameBuffer
+import traffic.jam.Main.Companion.dim
+
+/**
+ * The main goal of this class is to wrap operations that happens before and after using [SpriteBatch].
+ *
+ * The framebuffer is there in order to write to an image that is then scaled up to the screen size.
+ * It makes achieve a pixelated look more transparent for the rest of the code
+ */
+class Graphics {
+
+    val batch = SpriteBatch()
+    private val cam = OrthographicCamera(dim.wF, dim.hF)
+    private val frameBuffer = FrameBuffer(Pixmap.Format.RGBA8888, dim.w, dim.h, false)
+
+    init {
+        cam.position.set(dim.hwF, dim.hhF, 0f)
+        cam.update()
+    }
+
+    fun draw(drawingStuff: () -> Unit) {
+        // DRAW TO FRAMEBUFFER
+        frameBuffer.begin()
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+
+        batch.begin()
+        batch.projectionMatrix = cam.combined
+        Gdx.gl20.glEnable(GL20.GL_BLEND)
+        drawingStuff()
+        batch.end()
+        frameBuffer.end()
+
+        // ACTUALLY DRAW TO SCREEN
+        batch.color = Color.WHITE
+        batch.begin()
+        val texture = frameBuffer.colorBufferTexture
+        texture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
+        batch.draw(frameBuffer.colorBufferTexture, 0f, dim.hF, dim.wF, -dim.hF) // that's the flip
+        batch.end()
+
+        println("FPS: ${Gdx.graphics.framesPerSecond}")
+    }
+
+    fun dispose() {
+        batch.dispose()
+    }
+}
--- core/src/main/kotlin/traffic/jam/Main.kt
+++ core/src/main/kotlin/traffic/jam/Main.kt
@@ -2,54 +2,43 @@ package traffic.jam
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-
 import kotlinx.coroutines.*
 
 class Main : ApplicationAdapter() {
     private val things = Array(2000) { ThingThatMoves() }
     private val scope = CoroutineScope(Dispatchers.Default)
-    private lateinit var batch: SpriteBatch
     private lateinit var image: Texture
-    private lateinit var cam: OrthographicCamera
+    private lateinit var graphics: Graphics
+
     var state = State.START
 
     override fun create() {
-        batch = SpriteBatch()
         image = Texture("square.png")
-        cam = OrthographicCamera(dim.wF, dim.hF)
-        cam.position.set(dim.hwF, dim.hhF, 0f)
-        cam.update()
         Gdx.input.inputProcessor = InputHandler(this)
+        graphics = Graphics()
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.projectionMatrix = cam.combined
-        runBlocking {
-            if (state == State.PLAY)
-                things.forEach {
-                    scope.launch {
-                        it.move(Gdx.graphics.deltaTime)
-                        it.checkWallCollide(dim)
+        graphics.draw {
+            runBlocking {
+                if (state == State.PLAY)
+                    things.forEach {
+                        scope.launch {
+                            it.move(Gdx.graphics.deltaTime)
+                            it.checkWallCollide(dim)
+                        }
                     }
+                things.forEach {
+                    it.draw(graphics.batch, image)
                 }
-            things.forEach {
-                it.draw(batch, image)
             }
         }
-        batch.end()
-        println("FPS: ${Gdx.graphics.framesPerSecond}")
     }
 
     override fun dispose() {
         scope.cancel()
-        batch.dispose()
+        graphics.dispose()
         image.dispose()
     }
 
@@ -58,7 +47,7 @@ class Main : ApplicationAdapter() {
     }
 
     companion object {
-        const val SCALE = 10
+        const val SCALE = 5
         val dim: Dimension = Dimension(160, 144)
     }
 }
