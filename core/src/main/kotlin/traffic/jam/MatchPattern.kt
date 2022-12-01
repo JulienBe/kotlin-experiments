8e280e04cf8dde9f1562c72882dc195a71e0b135
Julien Bertozzi <bertozzijulien@gmail.com>
2022-12-01 05:47:08 +0100

    vertical patterns

--- core/src/main/kotlin/traffic/jam/MatchPattern.kt
+++ core/src/main/kotlin/traffic/jam/MatchPattern.kt
@@ -15,18 +15,22 @@ enum class MatchPattern(seedOffsets: Set<Set<Pos>>) {
             setOf(Pos(-2f, 0f), Pos(-1f, 0f), Pos(+1f, 0f), Pos(+2f, 0f)),
         )
     ),
+    VERTICAL_5(HORIZONTAL_5.swapXY()),
     HORIZONTAL_4(
         setOf(
             setOf(Pos(+1f, 0f), Pos(+2f, 0f), Pos(+3f, 0f)),
             setOf(Pos(-2f, 0f), Pos(-1f, 0f), Pos(+1f, 0f)),
         )
     ),
+    VERTICAL_4(HORIZONTAL_4.swapXY()),
     HORIZONTAL_3(
         setOf(
             setOf(Pos(-1f, 0f), Pos(-2f, 0f)),
             setOf(Pos(-1f, 0f), Pos(+1f, 0f)),
         )
-    );
+    ),
+    VERTICAL_3(HORIZONTAL_3.swapXY()),
+    ;
 
     val offsets: Set<Set<Pos>> = seedOffsets
         .flatMap { setOf(
@@ -34,4 +38,12 @@ enum class MatchPattern(seedOffsets: Set<Set<Pos>>) {
             it.map { pos -> Pos(-pos.xf, -pos.yf) }.toSet(),    // create the opposite offsets
         ) }
         .toSet()
+
+    private fun swapXY(): Set<Set<Pos>> {
+        return offsets.map { set ->
+            set.map { pos ->
+                Pos(pos.yf, pos.xf)
+            }.toSet()
+        }.toSet()
+    }
 }
